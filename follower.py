47b3fd39c71843cae476b3f15742c822f3dd550c
ubuntu <mythsman>
2016-08-26 11:12:48 +0800

    almost done

--- follower.py
+++ follower.py
@@ -1,16 +1,17 @@
 #encoding:utf-8
 from bs4 import BeautifulSoup 
 import urllib2,cookielib,urllib,json,sys,time,re,threading,random
-from  opener import Opener
+from  opener import getOpener
 
 def queryFollower(url):
-	urllib2.install_opener(Opener().getOpener())
+	urllib2.install_opener(getOpener())
 	while True:
 		try:
 			request=urllib2.urlopen(url)
 			break
 		except:
-			pass
+			print '403'
+	print 'done'
 	html=request.read()
 	html=html.replace('\n',' ')
 	data=json.loads(html)
@@ -40,8 +41,6 @@ def getFollowers(containerId):
 	user_url="http://m.weibo.cn/page/json?containerid="+containerId+"_-_FOLLOWERS"
 	data=queryFollower(user_url)
 	formated=json.dumps(data,indent=4)
-	f=open('response.txt','w')
-	f.write(formated.decode('unicode-escape').encode('utf-8'))
 	count=data['count']
 	page_count=(count-1)/10+1
 	timers=[]
@@ -57,26 +56,35 @@ def getFollowers(containerId):
 
 
 def getUser(uid):
-	urllib2.install_opener(Opener().getOpener())
-	user_url="http://m.weibo.cn/u/"+uid
-	request=urllib2.urlopen(user_url)
-	html=request.read()
-	html=html.replace('\n',' ')
-	config=re.findall(r'window.\$config=(.*?);',html)[0]
-	render=re.findall(r'window.\$render_data =(.*?);',html)[0]
-	config=config.replace(' ','')
-	config=config.replace('\'','\"')
-	render=render.replace(' ','')
-	render=render.replace('\'','\"')
-	config_decoded=json.loads(config)
-	render_decoded=json.loads(render)
-
-	containerId= render_decoded['common']['containerid']
-	Id=render_decoded['stage']['page'][1]['id']
-	description=render_decoded['stage']['page'][1]['description']
-	nativePlace=render_decoded['stage']['page'][1]['nativePlace']
-	name=render_decoded['stage']['page'][1]['name']
-	gender=render_decoded['stage']['page'][1]['ta']
+ 	while True:
+		urllib2.install_opener(getOpener())
+		user_url="http://m.weibo.cn/u/"+str(uid)
+		hasPage=False
+		try:
+			request=urllib2.urlopen(user_url)
+			hasPage=True
+		except:
+			print '403'
+			continue	
+		html=request.read()
+		html=html.replace('\n',' ')
+		config=re.findall(r'window.\$config=(.*?);',html)[0]
+		render=re.findall(r'window.\$render_data =(.*?);',html)[0]
+		config=config.replace(' ','')
+		config=config.replace('\'','\"')
+		render=render.replace(' ','')
+		render=render.replace('\'','\"')
+		config_decoded=json.loads(config)
+		render_decoded=json.loads(render)
+		containerId= render_decoded['common']['containerid']
+		Id=render_decoded['stage']['page'][1]['id']
+		description=render_decoded['stage']['page'][1]['description']
+		nativePlace=render_decoded['stage']['page'][1]['nativePlace']
+		name=render_decoded['stage']['page'][1]['name']
+		gender=render_decoded['stage']['page'][1]['ta']
+		if hasPage:
+			break
+	print 'done'
 	dicts={}
 	dicts['uid']=Id
 	dicts['containerId']=containerId
